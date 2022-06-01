38492d432a5345608c7e309ba9d620c3ac2f78e6
marcodeiulio <mdeiulio@icloud.com>
2022-06-01 16:54:46 +0200

    - Edited ex 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,25 +13,9 @@ namespace la_mia_pizzeria_static.Controllers
             _logger = logger;
         }
 
-        //2. Creare una funzione che stampa la somma dei numeri che vanno da 1 a n(con n passato come parametro - n incluso)
-        //Stampare dentro uno span il risultato.
-        public int SommaNum(int num)
-        {
-            int sum = 0;
-            for (int i = 0; i <= num; i++)
-            {
-                sum += i;
-            }
-            return sum;
-        }
-
-        
-
         public IActionResult Index()
         {
-            int sum = SommaNum(10);
-            
-            return View(sum);
+            return View();
         }
 
         public IActionResult Description()
